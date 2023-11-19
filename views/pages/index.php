451e0ee89ad417bcc012965c16b8735173bdb8ea
Suleymanov Artur <l0tus0rb@yandex.ru>
2023-11-19 14:20:21 +0200

    Added a warning if there are no films with the desired title

--- views/pages/index.php
+++ views/pages/index.php
@@ -7,6 +7,9 @@
 
 
 <div class="catalog">
+	<?php if(empty($movies)):?>
+		<p>There are no films with this name</p>
+	<?php endif;?>
 	<?php
 	foreach ($movies as $movie): ?>
 		<?= view('components/film-card', ['movie' => $movie]) ?>
