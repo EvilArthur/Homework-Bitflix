3eefc2f32dbde717eb1cedc2b4e721142d4d27bb
Suleymanov Artur <l0tus0rb@yandex.ru>
2023-12-04 22:39:03 +0200

    Reformat code

--- views/components/menu.php
+++ views/components/menu.php
@@ -10,12 +10,12 @@ $currentPage = $_SERVER['REQUEST_URI'];
 	<ul>
 		<?php
 		foreach ($items as $item):?>
-			<li class="menu-item"><a href="<?= $item['url'] ?>" class="menu-link <?=
-				((stripos($currentPage, substr($item['url'], 2)) !== false and $item['url'] !== '/') or
-					($currentPage === $item['url']))
-					? 'active' : ''?>">
+			<li class="menu-item"><a href="<?= $item['url'] ?>" class="menu-link <?= ((stripos(
+							$currentPage,
+							substr($item['url'], 2)
+						) !== false and $item['url'] !== '/') or ($currentPage === $item['url'])) ? 'active' : '' ?>">
 					<?= $item['text'] ?></a></li>
 		<?php
-		endforeach;?>
+		endforeach; ?>
 	</ul>
 </nav>
